a4cac1dfc924ed898795da66393c1e67b46f8ad4
Andrew Jenkins <andrewj@f5.com>
2015-02-03 14:24:17 -0700

    Make function name easier to grep for

--- 01-easy-benchmarks/03-monopoly.js
+++ 01-easy-benchmarks/03-monopoly.js
@@ -2,22 +2,23 @@ var Benchmark = require('benchmark');
 var monopoly = new Benchmark.Suite;
 var samples = parseInt(process.env.SAMPLES) || 100;
 
-function add(a, b) {
+function doTheAdd(a, b) {
   return a + b;
 }
 
 monopoly.add('monomorphic', function monomorphicAdd() {
-  add(1, 2);
-  add(3, 4);
+  doTheAdd(1, 2);
+  doTheAdd(3, 4);
 }, { minSamples : samples });
 
 monopoly.add('polymorphic', function polymorphicAdd() {
-  add(1, 2);
-  add('a', 'b');
+  doTheAdd(1, 2);
+  doTheAdd('a', 'b');
 }, { minSamples : samples });
 
 monopoly.add('nonemorphic', function nonemorphicAdd() {
   3;
+  'ab';
 }, { minSamples: samples });
 
 monopoly.on('cycle', function (event) {
